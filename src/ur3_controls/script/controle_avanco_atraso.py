70a057a1075987dd8819d76c4d766891d8514df6
rrmatos <ramosunb@gmail.com>
2021-08-15 12:09:56 -0300

    Adding codes to tg2 explanation

--- src/ur3_controls/script/controle_avanco_atraso.py
+++ src/ur3_controls/script/controle_avanco_atraso.py
@@ -11,19 +11,14 @@ class MyControl:
         # Para todas as juntas do ur3, da junta da base 
         # ate a junta do efetuador terminal, defina o estado inicial
         # aqui
-        self.qrk = [0, 0, 0, 0, 0, 0] # entrada de posicao Qr[k]  
-        self.qok = [0, 0, 0, 0, 0, 0] # saida de posicao Qo[k] 
+        self.Refence_Position = [0, 0, 0, 0, 0, 0] # entrada de posicao qr[k]  
+        self.Position = [0, 0, 0, 0, 0, 0] # saida de posicao qo[k] 
         
-        self.uk = [0, 0, 0, 0, 0, 0] # sinal de controle U[k] 
-        self.ukmenos1 = [0, 0, 0, 0, 0, 0] # sinal de controle U[k-1] 
+        self.Refence_Velocity = [0, 0, 0, 0, 0, 0] # sinal de controle u[k] 
+        self.Refence_Velocity_Old = [0, 0, 0, 0, 0, 0] # sinal de controle u[k-1] 
 
-        self.ek = [0, 0, 0, 0, 0, 0] # sinal de erro E[k]
-        self.ekmenos1 = [0, 0, 0, 0, 0, 0] # sinal de erro E[k-1] 
-        ##############################################################
-        ##############################################################
-        # velocidade referencia inicial;
-        self.ref_vel_msg = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        ##############################################################
+        self.Position_Erro = [0, 0, 0, 0, 0, 0] # sinal de erro e[k]
+        self.Position_Erro_Old = [0, 0, 0, 0, 0, 0] # sinal de erro e[k-1] 
         
     def control_law(self, robot_state, ref_pose_msg):
         ##############################################################
@@ -42,22 +37,22 @@ class MyControl:
         # for que gere todas as variaveis do controlador 
         for idx in range(6):
             # idx eh um interador que vai de 0 a 5:
-            self.qrk[idx] = ref_pose_msg.data[idx]
-            self.qok[idx] = robot_state.position[idx]
+            self.Refence_Position[idx] = ref_pose_msg.data[idx]
+            self.Position[idx] = robot_state.position[idx]
             # self.ek[idx]: sinal de erro de posicao para
             # cada junta(entra - saida)
-            self.ek[idx] = self.qrk[idx] - self.qok[idx]
+            self.Position_Erro[idx] = (self.Refence_Position[idx]
+             - self.Position[idx][idx])
             ###########################################################
             # Lei de controle para o controlador proposto
             # para todas as juntas 
-            self.uk[idx] = (0.958*self.ukmenos1[idx]
-             + 0.30067*self.ek[idx] - 0.2108*self.ekmenos1[idx])
+            self.Velocity[idx] = (0.958*self.Velocity_Old[idx]
+             + 0.30067*self.Position_Erro[idx]
+             - 0.2108*self.Position_Erro_Old[idx])
             ###########################################################
-            self.ukmenos1[idx] = self.uk[idx]
-            self.ekmenos1[idx] = self.ek[idx]
-
-            self.ref_vel_msg[idx] = self.uk[idx]
+            self.Velocity_Old[idx] = self.Velocity[idx]
+            self.Position_Erro_Old[idx] = self.Position_Erro[idx]
         ###############################################################
         # sinal de controle parada junta do robo(sinal de velocidade)
-        return self.ref_vel_msg
+        return  self.Velocity
         ###############################################################
